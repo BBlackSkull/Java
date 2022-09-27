e5c1354dd1ac7ba2fbd2b22926bb25cc0209848f
Yamall <yamall.avalos@gmail.com>
2022-09-27 19:30:37 -0300

    agregando usuario

--- Spring/demo/src/main/java/com/springDemo/demo/controllers/usuarioController.java
+++ Spring/demo/src/main/java/com/springDemo/demo/controllers/usuarioController.java
@@ -1,14 +1,20 @@
 package com.springDemo.demo.controllers;
 
+import com.springDemo.demo.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class usuarioController {
-@RequestMapping(value = "prueba")
-    public String prueba() {
+@RequestMapping(value = "usuario" )
+    public Usuario getUsuario() {
+    Usuario usuario = new Usuario();
+    usuario.setNombre("Yamall");
+    usuario.setApellido("Avalos");
+    usuario.setEmail("yamalls@example.com");
+    usuario.setPassword("123456");
 
-        return "prueba";
+        return usuario;
     }
 }
